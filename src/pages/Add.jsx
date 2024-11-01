6825b2093b7ec705a59a94ba67171872b7c1b255
shuhrat4 <xudoyberdiyevshuhrat5@gmail.com>
2024-11-01 12:44:30 +0500

    d

--- src/pages/Add.jsx
+++ src/pages/Add.jsx
@@ -11,17 +11,17 @@ function Add() {
   })
 
   function handleSubmit(e){
-    
+
   }
 
 
   return (
-    <from onSubmit={handleSubmit} className='w-[600px] space-y-5 mx-auto mt-5'>
-      <Input value={name} onChange={(e) => setName(e.target.value)} size='large' placeholder='Enter age' allowClear />
-      <Input value={surname} onChange={(e) => setSurname(e.target.value)} size='large' placeholder='Enter age' allowClear />
+    <div onSubmit={handleSubmit} className='w-[600px] space-y-5 mx-auto mt-5'>
+      <Input value={name} onChange={(e) => setName(e.target.value)} size='large' placeholder='Enter name' allowClear />
+      <Input value={surname} onChange={(e) => setSurname(e.target.value)} size='large' placeholder='Enter surname' allowClear />
       <Input value={age} onChange={(e) => setAge(e.target.value)} size='large' placeholder='Enter age' allowClear />
       <Button htmlType='submit' className='w-full' size='large' type='primary'>Submit</Button>
-    </from>
+    </div>
   );
 }
 
